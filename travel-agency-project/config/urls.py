9e108c4bbe2c33c11be23539c754d7f09a0e025b
Aditya Vispute <154086246+Adityasonar29@users.noreply.github.com>
2026-08-02 23:40:35 +0530

    Add name to home URL path in urls.py

--- travel-agency-project/config/urls.py
+++ travel-agency-project/config/urls.py
@@ -5,7 +5,7 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('apps.core.urls')),
+    path('', include('apps.core.urls'), name='home'),
     path('booking/', include('apps.booking.urls')),
 ]
 

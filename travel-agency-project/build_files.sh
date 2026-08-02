0212b6e19872e95c787a23e58acf69fc941d045d
Aditya Vispute <154086246+Adityasonar29@users.noreply.github.com>
2026-08-02 23:30:53 +0530

    Delete travel-agency-project/build_files.sh

--- travel-agency-project/build_files.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-echo "Building project..."
-# Add the --break-system-packages flag to bypass PEP 668 restriction
-python3 -m pip install -r requirements.txt --break-system-packages
-
-echo "Collecting static files..."
-python3 manage.py collectstatic --noinput --clear

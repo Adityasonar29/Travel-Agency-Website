8bf5b5ee2f49b01276fe52e148a73bfff2dbc2c7
Aditya Vispute <154086246+Adityasonar29@users.noreply.github.com>
2026-08-02 23:28:53 +0530

    Update build script to bypass PEP 668 restriction
    Added the --break-system-packages flag to pip install command to bypass PEP 668 restriction.

--- travel-agency-project/build_files.sh
+++ travel-agency-project/build_files.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
 echo "Building project..."
-python3 -m pip install -r requirements.txt
+# Add the --break-system-packages flag to bypass PEP 668 restriction
+python3 -m pip install -r requirements.txt --break-system-packages
 
 echo "Collecting static files..."
 python3 manage.py collectstatic --noinput --clear
